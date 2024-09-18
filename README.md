a36d8d59af94d8f79350ee8cd2deb2b6c9358aa3
rupambora31 <iamrupam99@gmail.com>
2024-09-18 08:34:00 +0530

    complete initial set-up

---
+++ README.md
@@ -0,0 +1,1 @@
+Xongroh: A Social MarketPlace for Creators.
---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import SignInForm from './_auth/forms/SignInForm';
+import SignUpForm from './_auth/forms/SignUpForm';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
+import { Home } from './_root/pages';
+
+import './globals.css';
+import { Route, Routes } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* Public */}
+        <Route element={<AuthLayout />}>
+          <Route path="sign-in" element={<SignInForm />} />
+          <Route path="sign-up" element={<SignUpForm />} />
+        </Route>
+
+        {/* Private */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
+};
+
+export default App;
