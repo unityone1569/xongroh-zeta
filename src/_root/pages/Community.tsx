6fb121d6be034cd001ef2384a2e52fdfd33614e6
rupambora31 <iamrupam99@gmail.com>
2024-12-09 21:35:23 +0530

    refactor Community and Marketplace pages; update flex alignment for better layout consistency

--- src/_root/pages/Community.tsx
+++ src/_root/pages/Community.tsx
@@ -3,8 +3,8 @@ import VoteButton from "@/components/shared/VoteButton";
 
 const Community = () => {
   return (
-    <div className="flex-col flex-1 items-center overflow-scroll py-10 px-6 md:p-14 custom-scrollbar">
-      <div className="max-w-5xl flex flex-col items-center w-full h-full gap-6 md:gap-9">
+    <div className="flex-col flex-1 flex-center overflow-scroll py-10 px-6 md:p-14 custom-scrollbar">
+      <div className="max-w-5xl flex flex-col  w-full h-full gap-6 md:gap-9">
         <h2 className="h3-bold md:h2-bold w-full">Community</h2>
 
         <div className="flex flex-col items-center h-full justify-center gap-4 pb-10">
