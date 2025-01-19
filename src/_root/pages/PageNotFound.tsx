453bb6049b0be3e2afef181ab685f0b894271b46
rupambora31 <iamrupam99@gmail.com>
2025-01-19 21:46:51 +0530

    feat: enhance 404 page with new image and updated messaging for better user experience

--- src/_root/pages/PageNotFound.tsx
+++ src/_root/pages/PageNotFound.tsx
@@ -1,21 +1,26 @@
+import LazyImage from '@/components/shared/LazyImage';
 import { Link } from 'react-router-dom';
 
 const PageNotFound = () => {
   return (
     <div className="flex flex-col items-center w-full justify-center h-screen text-gray-800">
       <div className="text-center border border-dark-4 m-5 mx-9 py-16 bg-dark-3 rounded-3xl">
-        <h1 className="text-6xl font-bold text-primary-500 mb-4">404</h1>
-        <h3 className="text-2xl text-light-2 font-semibold mb-2">
-          Page Not Found
+        <LazyImage
+          src="/assets/icons/404.png"
+          alt="post"
+          className="mb-5"
+        />
+        <h3 className="h3-bold md:h1-bold text-light-2 mb-5">
+          Why are you here?
         </h3>
-        <p className="text-light-3 mb-11 mx-10">
-          Sorry, the page you are looking for does not exist or has been moved.
+        <p className="text-light-3 small-regular md:base-regular mb-11 mx-10">
+          You're not supposed to be here.
         </p>
         <Link
           to="/"
           className="px-5 py-3 text-dark-1 font-medium bg-purple-300 rounded-lg shadow-md hover:bg-purple-400 transition"
         >
-          <span className="pr-1.5 items-center">&larr;</span> Back to Homepage
+          <span className="pr-1.5 items-center">&larr;</span> Go home
         </Link>
       </div>
     </div>
