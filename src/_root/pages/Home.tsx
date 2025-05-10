2a0590e4ad3c2df8ef6a5964b9aaf14c4c091ace
rupambora31 <iamrupam99@gmail.com>
2025-05-10 15:12:04 +0530

    style: adjust margin for COTM section in Home component

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -276,13 +276,8 @@ const Home = () => {
     <div className="home-container">
       <div className="home-posts">
         <div className="flex flex-col gap-9 w-full">
-          <div className="flex flex-col gap-4 mt-16 lg:mt-0">
-            <div className="flex flex-col gap-4 mt-3">
-              <div className="flex items-center">
-                <h2 className="text-primary-500 h3-bold sm:h2-bold pb-6">
-                  COTM - Competition
-                </h2>
-              </div>
+          <div className="flex flex-col gap-4 mt-16 lg:mt-5">
+            <div className="flex flex-col gap-4 mt-1">
               <COTMCarousel />
 
               {/* Winner info and countdown section */}
