c5f98561c2ecf0a0d39d38d76059a8dc8de8ed42
rupambora31 <iamrupam99@gmail.com>
2025-05-12 16:06:59 +0530

    style: update community name text style for improved emphasis

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -234,7 +234,7 @@ const Home = () => {
                     })`,
                   }}
                 />
-                <span className="text-light-3 text-xs truncate w-20 text-center pb-3">
+                <span className="text-light-3 subtle-comment-semibold md:small-semibold truncate w-20 text-center pb-3">
                   {community.name}
                 </span>
               </Link>
